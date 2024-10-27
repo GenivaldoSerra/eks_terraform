65986bcb1c523eec629d0327e5e34d744fbce7db
Genivaldo <genivaldo.serra@live.com>
2024-10-27 08:14:07 -0300

    feat: configurações iniciais da VPC e EKS

---
+++ src/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+
+  name = "eks-terraform-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.26.1"
+
+  cluster_name    = "eks-terraform"
+  cluster_version = "1.31"
+
+  enable_cluster_creator_admin_permissions = true
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.default_vpc_id
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+      instance_types = ["t2.micro"]
+    }
+  }
+}
